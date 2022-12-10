e73e436fc3e980f0ecee5426b1e880d77ab31ac0
CarlosEduardo1984 <33332202+carloseduardo1984@users.noreply.github.com>
2022-12-10 14:12:01 +0000

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -3,35 +3,35 @@ import './commands'
 
 require('cypress-xpath')
 
-afterEach(() => {
-    const screenshotsFolder = Cypress.config("screenshotsFolder");
-    if (window.cucumberJson?.generate) {
-        const testState = window.testState;
-        const stepResult =
-            testState.runTests[testState.currentScenario.name][testState.currentStep];
-        if (stepResult?.status === "failed") {
-            const screenshotFileName = `${testState.feature.name} -- ${testState.currentScenario.name} (failed).png`;
-            cy.readFile(
-                `${screenshotsFolder}/${Cypress.spec.name}/${screenshotFileName}`,
-                "base64"
-            ).then((imgData) => {
-                stepResult.attachment = {
-                    data: imgData,
-                    media: { type: "image/png" },
-                    index: testState.currentStep,
-                    testCase: testState.formatTestCase(testState.currentScenario),
-                };
-            });
+// afterEach(() => {
+//     const screenshotsFolder = Cypress.config("screenshotsFolder");
+//     if (window.cucumberJson?.generate) {
+//         const testState = window.testState;
+//         const stepResult =
+//             testState.runTests[testState.currentScenario.name][testState.currentStep];
+//         if (stepResult?.status === "failed") {
+//             const screenshotFileName = `${testState.feature.name} -- ${testState.currentScenario.name} (failed).png`;
+//             cy.readFile(
+//                 `${screenshotsFolder}/${Cypress.spec.name}/${screenshotFileName}`,
+//                 "base64"
+//             ).then((imgData) => {
+//                 stepResult.attachment = {
+//                     data: imgData,
+//                     media: { type: "image/png" },
+//                     index: testState.currentStep,
+//                     testCase: testState.formatTestCase(testState.currentScenario),
+//                 };
+//             });
 
-        }
-        else{
-            console.log(testState.feature.name)
-            console.log(stepResult?.title)
-            console.log(stepResult?.status)
-        }
-    }
+//         }
+//         else{
+//             console.log(testState.feature.name)
+//             console.log(stepResult?.title)
+//             console.log(stepResult?.status)
+//         }
+//     }
 
-});
+// });
 
 Cypress.on('fail', (err) => {
     console.error(err)
