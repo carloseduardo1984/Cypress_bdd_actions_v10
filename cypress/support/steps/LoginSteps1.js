d4812ae6ffc71bbeb4518fda14fac5049c9fdf70
CarlosEduardo1984 <33332202+carloseduardo1984@users.noreply.github.com>
2022-12-10 16:20:22 +0000

    Update LoginSteps1.js

--- cypress/support/steps/LoginSteps1.js
+++ cypress/support/steps/LoginSteps1.js
@@ -5,12 +5,15 @@ const loginPage = new LoginPage
 
 Given("acesso o site ultima", () => {
     //loginPage.acessarSite();
+    cy.log('step given ');
 })
 
 When("acesso a pagina de login", () => {
     //loginPage.abraPaginaLogin();
+    cy.log('step when ');
 })
 
 Then("devo visualizar botao de conecte", () => {
     //loginPage.visualizarBotaoCadastro();
+    cy.log('step then ');
 })
